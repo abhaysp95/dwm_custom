d104961f7297e762b378dc7d43fa71182402ebe2
coolabhays <abhaysp9955@gmail.com>
2021-03-21 16:55:45 +0530

    changed default main terminal to kitty

--- config.h
+++ config.h
@@ -123,7 +123,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-i", "-p", "exec:", "-c", "-l", "25", NULL };
-static const char *termcmd[]  = { "alacritty" , NULL };
+static const char *termcmd[]  = { "kitty" , NULL };
 
 /* User defined commnads */
 static const char* sectermcmd[] = { "termite", NULL };
