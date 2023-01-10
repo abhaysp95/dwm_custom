1ddfb3d28558b13864083a2dc5344439928d66a3
abhay <abhaysp9955@gmail.com>
2023-01-10 17:08:53 +0530

    chore: minor update

--- config.h
+++ config.h
@@ -15,11 +15,11 @@ static unsigned int gappoh    		= 8;       /* horiz outer gap between windows an
 static unsigned int gappov    		= 8;       /* vert outer gap between windows and screen edge */
 static int smartgaps          		= 1;        /* 1 means no outer gap when there is only one window */
 static int showbar                  	= 1;        /* 0 means no bar */
-static int topbar                   	= 1;        /* 0 means bottom bar */
+static int topbar                   	= 0;        /* 0 means bottom bar */
 static const int user_bh            = 16;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
-static const char *fonts[]          	= { "UbuntuMono Nerd Font Mono:style=Regular:size=10" };
+static const char *fonts[]          	= { "UbuntuMono Nerd Font Mono:style=Regular:size=9" };
 static const char dmenufont[]       	= "monospace:size=10";
 static char normbgcolor[]           	= "#222222";
 static char normbordercolor[]       	= "#444444";
